ad6de26c811b94359cf6c7b0950b1bac8bee50dc
AzizGerges <201927170@std.sci.cu.edu.eg>
2023-03-03 19:57:27 +0200

    test2

--- KidzoApp/App.js
+++ KidzoApp/App.js
@@ -9,6 +9,9 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
