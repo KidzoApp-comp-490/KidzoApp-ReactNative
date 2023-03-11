25d407568000f37d9eab9f145b7865b59c0d719d
AzizGerges <201927170@std.sci.cu.edu.eg>
2023-03-11 23:00:48 +0200

    finish navigation at all pages

--- KidzoApp/App.js
+++ KidzoApp/App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import SingIn from "./Components/Users/Sign";
-import SingUp from "./Components/Users/SingUp"
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import SingIn from "./Components/Users/SignIn";
+import SingUp from "./Components/Users/SingUp";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ForgetPass from "./Components/Users/ForgetPass";
+import Confirm from "./Components/Users/Confirm";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="SingIn">
-    <Stack.Screen name="SingIn" component={SingIn} />
-    <Stack.Screen name="SingUp" component={SingUp} />
-        </Stack.Navigator>
+      <Stack.Navigator initialRouteName="SingIn">
+        <Stack.Screen
+          name="SingIn"
+          component={SingIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SingUp"
+          component={SingUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ForgetPass"
+          component={ForgetPass}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Confirm"
+          component={Confirm}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
